350fccf7f5876b82141ca1b5e57e2b9193c281e9
aleexwae4-lab <aleexwae4@gmail.com>
2026-09-08 18:34:14 -0600

    V21.8 activate fixed-origin joystick controller

--- src/v21/boot.js
+++ src/v21/boot.js
@@ -3,4 +3,4 @@ import './shell.css';
 import './shell.js';
 import './premium-polish.css';
 import './controls-refinement.css';
-import './flight-controls-v217.js';
+import './flight-controls-v218.js';
